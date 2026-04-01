2c9f10aa60a9ec6e68e8a94b7e7ad79f1109d903
Nerevakiin <djmatisk@yahoo.gr>
2026-04-02 00:17:27 +0300

    making of the first list

--- first-react/src/index.jsx
+++ first-react/src/index.jsx
@@ -1,7 +1,18 @@
 import { createRoot } from 'react-dom/client'
+import reactSvg from './assets/react.svg'
 
 const root = createRoot(document.getElementById("root"))
 
 root.render(
-  <h1>hello there!</h1>
+  <main>
+    <img src={reactSvg} width="40px" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first release in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 200K stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </main>
 )
