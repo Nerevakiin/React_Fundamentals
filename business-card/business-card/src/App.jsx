b27e3726e19d3d79b05b3ec38d5735c9d9a8ad16
Nerevakiin <djmatisk@yahoo.gr>
2026-04-04 02:49:35 +0300

    basic foundation unstyled

--- business-card/business-card/src/App.jsx
+++ business-card/business-card/src/App.jsx
@@ -1,6 +1,63 @@
+
+
+function BasicInfo() {
+  return (
+    <>
+      <img src="./king-crimson.jpg" />
+      <h2>Stamatis Karasavvidis</h2>
+      <h3>Full-Stack Developer</h3>
+      <a href="https://github.com/Nerevakiin/" target="_blank">Portfolio</a>
+    </>
+  )
+}
+
+function Buttons() {
+  return (
+    <>
+      <div className="buttons">
+        <a href="mailto:djmatisk@yahoo.gr" className="email-btn">Send Email</a>
+        <a href="https://www.linkedin.com/in/stamatiskarasavvidis/" className="linkedin-btn">LinkedIn</a>
+      </div>
+    </>
+  )
+}
+
+function Main() {
+  return (
+    <>
+      <h3>About</h3>
+      <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
+        I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+
+      <h3>Interests</h3>
+      <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </>
+  )
+}
+
+
+function Footer() {
+  return (
+    <>
+      <a>Facebook</a>
+      <a>Twitter</a>
+      <a>Instagram</a>
+      <a>Github</a>
+    </>
+    
+  )
+}
+
+
 export default function App() {
   return (
-    <h1>root render here</h1>
+    <>
+      <BasicInfo />
+      <Buttons />
+      <Main />
+      <Footer />
+      <h1>root render here!!</h1>
+    </>
   )
 }
 
