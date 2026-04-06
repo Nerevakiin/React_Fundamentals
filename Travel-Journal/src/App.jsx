5b3ac3571d53ff3c03d0863fb4a87d92231dce48
Nerevakiin <djmatisk@yahoo.gr>
2026-04-06 19:08:54 +0300

    data driven react start

---
+++ Travel-Journal/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+
+
+
+
+export default function App() {
+  return(
+    <>
+      <Header />
+      <h1>This is the Main App component</h1>
+    </>
+  )
+}
---
+++ Travel-Journal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <header>
+                <img src="./globe.png" alt="globe" />
+                <span>my travel journal.</span>
+            </header>
+        </>
+    )
+}
