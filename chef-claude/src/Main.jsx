dbd0f2e2c3900411fa87653c116780cb42dbb009
Nerevakiin <djmatisk@yahoo.gr>
2026-04-17 01:29:00 +0300

    black magic stuff
    arrays

--- chef-claude/src/Main.jsx
+++ chef-claude/src/Main.jsx
@@ -1,26 +1,41 @@
+import React from 'react'
+
 export default function Main() {
 
+
+    const [ingredients, setIngredients] = React.useState([])
+    
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
     
     
-    function handleClick(e) {
-        console.log("clicked!")
+    
+    
+    function handleSubmit(e) {
+        
         e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const newIngredient = formData.get("ingredient")
+
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-
-
 
 
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                 aria-label="add-ingredient"
                 type="text"
                 placeholder="e.g. oregano"
                 name="ingredient"
                 />
-                <button onClick={handleClick}>Add ingredient</button>
+                <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
