7fe14d66daa84b20d3dde3b6ec67caee77e6a9bb
Nerevakiin <djmatisk@yahoo.gr>
2026-04-16 05:04:38 +0300

    button

--- chef-claude/src/Main.jsx
+++ chef-claude/src/Main.jsx
@@ -1,4 +1,15 @@
 export default function Main() {
+
+    
+    
+    function handleClick(e) {
+        console.log("clicked!")
+        e.preventDefault()
+    }
+
+
+
+
     return (
         <main>
             <form className="add-ingredient-form">
@@ -6,8 +17,9 @@ export default function Main() {
                 aria-label="add-ingredient"
                 type="text"
                 placeholder="e.g. oregano"
+                name="ingredient"
                 />
-                <button>Add ingredient</button>
+                <button onClick={handleClick}>Add ingredient</button>
             </form>
         </main>
     )
