a4e6b1edfc66533d03b2ce47b7af05646eaf4795
Nerevakiin <djmatisk@yahoo.gr>
2026-04-23 02:47:51 +0300

    proper conditional rendering

--- chef-claude/src/Main.jsx
+++ chef-claude/src/Main.jsx
@@ -40,7 +40,8 @@ export default function Main() {
                 <button>Add ingredient</button>
             </form>
 
-            <section>
+            { ingredients.length > 0 &&
+                <section>
                 <h2>Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">
                     {ingredientsListItems}
@@ -53,6 +54,7 @@ export default function Main() {
                     <button>Get a recipe</button>
                 </div>
             </section>
+            }
 
         </main>
     )
