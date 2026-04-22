1e2871fc096b25874a8adc8e89878ffe0aaafe4e
Nerevakiin <djmatisk@yahoo.gr>
2026-04-23 02:40:55 +0300

    conditional rendering + styling

--- chef-claude/src/Main.jsx
+++ chef-claude/src/Main.jsx
@@ -4,16 +4,16 @@ export default function Main() {
 
 
     const [ingredients, setIngredients] = React.useState([])
-    
+
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
-    
-    
-    
-    
+
+
+
+
     // function handleSubmit(e) {
-        
+
     //     e.preventDefault()
     //     const formData = new FormData(e.currentTarget)
     //     const newIngredient = formData.get("ingredient")
@@ -31,17 +31,29 @@ export default function Main() {
         <main>
             {/* <form onSubmit={handleSubmit} className="add-ingredient-form"> */}
             <form action={addIngredient} className="add-ingredient-form">
-                <input 
-                aria-label="add-ingredient"
-                type="text"
-                placeholder="e.g. oregano"
-                name="ingredient"
+                <input
+                    aria-label="add-ingredient"
+                    type="text"
+                    placeholder="e.g. oregano"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+
+            <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">
+                    {ingredientsListItems}
+                </ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
+
         </main>
     )
 }
