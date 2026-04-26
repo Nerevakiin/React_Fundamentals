7a1e3c67132c610d7e2e1aa20f64e915bae3e286
Nerevakiin <djmatisk@yahoo.gr>
2026-04-26 18:17:08 +0300

    ai functionality.
    added claude and qwen model for recipe generation

---
+++ chef-claude/src/ai.js
@@ -0,0 +1,63 @@
+import Anthropic from '@anthropic-ai/sdk'
+// import {query} from '@qwen-code/sdk'
+import dotenv from 'dotenv'
+
+
+const SYSTEM_PROMPT = `
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all 
+of those ingredients, You don't need to use every ingredient they mention in your recipe. The recipe can include additional
+ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it
+easier to render to a web page.
+`
+
+const anthropic = new Anthropic({
+    apiKey: import.meta.env.ANTHROPIC_API_KEY,
+    dangerouslyAllowBrowser: true // === IMPORTANT!! CHANGE THIS BEFORE DEPLOYING !! ===
+})
+
+
+export async function getRecipeFromChefClaude(ingredientsArr) {
+    const ingredientsString = ingredientsArr.join(", ")
+
+
+    const msg = await anthropic.messages.create({
+        model: 'claude-haiku-4-5-20251001',
+        max_tokens: 1024,
+        system: SYSTEM_PROMPT,
+        messages: [
+            {role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`},
+        ]
+    })
+
+    return msg.content[0].text 
+
+}
+
+
+// const qwen = new QWEN({
+//     apiKey: process.env.QWEN_API_KEY,
+//     dangerouslyAllowBrowser: true 
+// })
+
+// export async function getRecipeFrom Qwen(ingredientsArr) {
+//     const ingredientsString = ingredientsArr.join(", ")
+
+//     try {
+        
+//         const response = query({
+//             prompt: SYSTEM_PROMPT,
+//             messages: [
+//                 {role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`},
+//             ]
+
+//         })
+
+//         return response.content[0].text 
+
+
+//     } catch (err) {
+//         console.error(err.messages)
+//     }
+// }
+
+
