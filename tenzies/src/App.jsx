f815ce1deb8d9cf72f4a46e05efde9e0f19bed06
Nerevakiin <djmatisk@yahoo.gr>
2026-05-01 20:59:59 +0300

    hardcoded and stylized dice values

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,9 +1,21 @@
 
+import Die from './Die.jsx'
 
 export default function App() {
   return (
     <main>
-      <h1>TEST</h1>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={4} />
+        <Die value={1} />
+        <Die value={4} />
+        <Die value={1} />
+        <Die value={3} />
+        <Die value={2} />
+        <Die value={1} />
+      </div>
     </main>
   )
 }
