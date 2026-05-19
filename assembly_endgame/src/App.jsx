8fc3f9e76af6027d6f69bd4f81c846b342a640d7
Nerevakiin <djmatisk@yahoo.gr>
2026-05-19 23:20:26 +0300

    .

--- assembly_endgame/src/App.jsx
+++ assembly_endgame/src/App.jsx
@@ -9,6 +9,11 @@ export default function AssemblyEndgame() {
               <h1>Assembly: Endgame</h1>
               <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
             </header>
+
+            <section className="game-status">
+                <h2>You Win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
         </main>
     )
 }
