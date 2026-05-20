bbfd02678c6292e36a63d5fa8bb1c6dd2c204df0
Nerevakiin <djmatisk@yahoo.gr>
2026-05-21 00:58:25 +0300

    .

--- assembly_endgame/src/App.jsx
+++ assembly_endgame/src/App.jsx
@@ -1,17 +1,40 @@
 import React from "react"
+import { languages } from "./languages.js"
 
 
 
 export default function AssemblyEndgame() {
-    
-    
-    
-    
+
+    const languageArray = languages.map((language) => {
+
+        const styles = {
+            backgroundColor: language.backgroundColor,
+            color: language.color
+        }
+
+        return (
+            <span style={styles} className="language-element" key={language.name}>{language.name}</span>
+        )
+    })
+
+
+    // Set the state for the current word
+    const [currentWord, setCurrentWord] = React.useState("react")
+
+    // transform the string into an array and then map it and return the display of each letter
+    const letterElements = currentWord.split('').map((letter) => {
+        return (
+            <span>{letter}</span>
+        )
+    })
+
+
+
     return (
         <main>
             <header>
-              <h1>Assembly: Endgame</h1>
-              <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
             </header>
 
             <section className="game-status">
@@ -19,8 +42,12 @@ export default function AssemblyEndgame() {
                 <p>Well done! 🎉</p>
             </section>
 
-            <section>
+            <section className="language-list">
+                {languageArray}
+            </section>
 
+            <section className="word">
+                {letterElements}
             </section>
         </main>
     )
