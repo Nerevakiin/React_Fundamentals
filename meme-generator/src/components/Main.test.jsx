b59f8f1f18f6775b70637d8a4825830122aadafd
Nerevakiin <djmatisk@yahoo.gr>
2026-06-10 22:20:28 +0300

    Update Main.test.jsx

--- meme-generator/src/components/Main.test.jsx
+++ meme-generator/src/components/Main.test.jsx
@@ -1,15 +1,33 @@
-import {test, expect} from 'vitest'
-import {render, screen} from '@testing-library/react'
+import { test, expect, describe } from 'vitest'
+import { render, screen } from '@testing-library/react'
 
 import Main from './Main'
 
-test("displays the component", () => {
-  render(<Main/>)
 
-  expect(screen.getByText("One does not simply")).toBeInTheDocument()
-  expect(screen.getByText("Walk into Mordor")).toBeInTheDocument()
-  expect(screen.getByRole("img").src).toBe("https://i.imgflip.com/1bij.jpg")
+
+describe("Main", () => {
+
+  test("displays the component", () => {
+    render(<Main />)
+
+    expect(screen.getByText("One does not simply")).toBeInTheDocument()
+    expect(screen.getByText("Walk into Mordor")).toBeInTheDocument()
+    expect(screen.getByRole("img").src).toBe("https://i.imgflip.com/1bij.jpg")
+  })
+
+  test("Check if everything in the UI works as expected", () => {
+    render(<Main />)
+
+    expect(screen.getByText("Top Text")).toBeInTheDocument()
+    expect(screen.getByText("Bottom Text")).toBeInTheDocument()
+    expect(screen.getByPlaceholderText("One does not simply")).toBeInTheDocument()
+    expect(screen.getByPlaceholderText("Walk into Mordor")).toBeInTheDocument()
+    expect(screen.getByRole("button")).toBeInTheDocument()
+
+  })
+
 })
+
 
 
 
