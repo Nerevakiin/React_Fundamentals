ed3844d9ea200c12e9d65e6accb5ed89d69d10f9
Nerevakiin <djmatisk@yahoo.gr>
2026-06-10 22:43:03 +0300

    Update App.test.jsx

--- meme-generator/src/App.test.jsx
+++ meme-generator/src/App.test.jsx
@@ -5,34 +5,39 @@ import { render, screen } from '@testing-library/react'
 import App from './App.jsx'
 
 
-test("updates the top text", async () => {
-    
-    // Arrange
-    const user = userEvent.setup()
-    render(<App/>)
-    const topTextbox = screen.getAllByRole('textbox')[0]
+describe("user acceptance testing", () => {
 
-    // Act
-    await user.clear(topTextbox)
-    await user.type(topTextbox, "A coder does not simply")
+    test("updates the top text", async () => {
 
-    // Assert
-    expect(screen.getByText("A coder does not simply")).toBeInTheDocument()
+        // Arrange
+        const user = userEvent.setup()
+        render(<App />)
+        const topTextbox = screen.getAllByRole('textbox')[0]
+
+        // Act
+        await user.clear(topTextbox)
+        await user.type(topTextbox, "A coder does not simply")
+
+        // Assert
+        expect(screen.getByText("A coder does not simply")).toBeInTheDocument()
+
+    })
+
+    test("update the bottom text", async () => {
+
+        // Arange
+        const user = userEvent.setup()
+        render(<App />)
+        const bottomTextbox = screen.getAllByRole('textbox')[1]
+
+        // Act
+        await user.clear(bottomTextbox)
+        await user.type(bottomTextbox, "Code without coffee")
+
+        // Assert
+        expect(screen.getByText("Code without coffee")).toBeInTheDocument()
+
+    })
 
 })
 
-test("update the bottom text", async () => {
-
-    // Arange
-    const user = userEvent.setup()
-    render(<App/>)
-    const bottomTextbox = screen.getAllByRole('textbox')[1]
-
-    // Act
-    await user.clear(bottomTextbox)
-    await user.type(bottomTextbox, "Code without coffee")
-
-    // Assert
-    expect(screen.getByText("Code without coffee")).toBeInTheDocument()
-
-})
