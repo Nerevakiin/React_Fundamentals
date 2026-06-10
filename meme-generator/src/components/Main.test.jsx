6ec724a9a3f222a1191180725873ab79a0146dd6
Nerevakiin <djmatisk@yahoo.gr>
2026-06-10 21:45:50 +0300

    Update Main.test.jsx

--- meme-generator/src/components/Main.test.jsx
+++ meme-generator/src/components/Main.test.jsx
@@ -5,6 +5,10 @@ import Main from './Main'
 
 test("displays the component", () => {
   render(<Main/>)
+
+  expect(screen.getByText("One does not simply")).toBeInTheDocument()
+  expect(screen.getByText("Walk into Mordor")).toBeInTheDocument()
+  expect(screen.getByRole("img").src).toBe("https://i.imgflip.com/1bij.jpg")
 })
 
 
