5155dab5ae499aa406ab159517bb0298b3f2279e
Nerevakiin <djmatisk@yahoo.gr>
2026-06-11 21:38:40 +0300

    del

---
+++ meme-generator/src/a11y.test.jsx
@@ -0,0 +1,9 @@
+import { test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import App from './App.jsx'
+
+test("ensures troll face image is accessible", () => {
+    render(<App/>)
+    expect(screen.getByAltText("Troll face")).toBeInTheDocument()
+})
